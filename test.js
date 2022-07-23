fb1716eb94be1c5f50c883bc926c2f00b56d3fb7
lxw-peter <1270542959@qq.com>
2022-07-23 23:08:00 +0800

    调整目录

--- test.js
+++
@@ -1,5 +0,0 @@
-function fn(a, b) {
-  console.log(a, b)
-     return a + b
-}
-console.log(fn(3 + 4));
---
+++ vim/test.js
@@ -0,0 +1,12 @@
+function fn(test1, test2) {
+  console.log(test1, test2);
+
+  return test1 + test2;
+}
+
+function name({ ssssssaaa = 1, aaaaaaaaaaaab, aaaaaaaaaaaac = 3, aaaaaaaaaaad, aaaaaaaaaaaaae }) {
+  console.log([1]);
+  console.log(2);
+}
+
+console.log(fn(3 + 4));

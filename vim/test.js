75cdcc6e5100f010d4afe9a3cd0551996fbe0c3c
lxw-peter <1270542959@qq.com>
2022-07-29 00:13:07 +0800

    更新 22-23

--- vim/test.js
+++ vim/test.js
@@ -4,9 +4,17 @@ function fn(test1, test2) {
   return test1 + test2;
 }
 
-function name({ ssssssaaa = 1, aaaaaaaaaaaab, aaaaaaaaaaaac = 3, aaaaaaaaaaad, aaaaaaaaaaaaae }) {
+function getName({
+  ssssssaaa = 1,
+  aaaaaaaaaaaab,
+  aaaaaaaaaaaac = 3,
+  aaaaaaaaaaad,
+  aaaaaaaaaaaaae,
+}) {
   console.log([1]);
   console.log(2);
 }
 
 console.log(fn(3 + 4));
+
+fn(1, 2);
